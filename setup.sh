e572ecbe9a525f56cc8255d559e1f208fb3d2cca
liutong <liutong@zmeng123.com>
2019-01-17 14:58:15 +0800

    Add zsh history config.

--- setup.sh
+++ setup.sh
@@ -8,3 +8,10 @@ ln -sf  "$SCRIPT_DIR/vimrc"              ~/.vimrc
 ln -sf  "$SCRIPT_DIR/tmux.conf"              ~/.tmux.conf
 ln -sf  "$SCRIPT_DIR/zshrc"             ~/.zshrc
 ln -sf  "$SCRIPT_DIR/zsh_fzf_extra"             ~/.zsh_fzf_extra
+
+# git config
+git config --global user.email "437341974@qq.com"
+git config --global user.name "Liu Tong"
+# avoid enter password again and again
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
